e9479e2afbd945ea8c1b85081c3d89d33ffb43b2
Anthony <tony.gorski13@gmail.com>
2019-07-26 14:31:29 +0200

    Add proptypes + css

--- src/components/Room/index.js
+++ src/components/Room/index.js
@@ -1,23 +1,41 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './index.css';
 
 const Rooms = ({elements}) => {
 // le liveshare est toujours là??
 return (
-<div className="room-component">
-<ul>
-{
-elements.map(({name, adress}) => (
-<li>
-<p>Hello : {name}</p>
-<p>Address : {adress} </p>
-</li>
-))
-}
-</ul>
-</div>
+  <div className="container">
+    {
+      elements.map(({name, image,  adress, id, city, description,night_price, currency}) => (
+        <div key={id} className="room-list">
+          <img src={image} alt={id + "-img"} className="image"/>
+          <p><strong>Host</strong>: {name}</p>
+          <p><strong>Address</strong>: {adress}</p>
+          <p><strong>City</strong>: {city}</p>
+          <p><strong>Description</strong>: {description}</p>
+          <p><strong>Night price</strong>: {night_price}{currency}</p>
+        </div>
+      ))
+    }
+  </div>
 
 )
 }
+
+Rooms.propTypes = {
+  elements: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      image: PropTypes.string,
+      adress: PropTypes.string,
+      id: PropTypes.string,
+      city: PropTypes.string,
+      description: PropTypes.string,
+      night_price: PropTypes.number,
+      currency: PropTypes.string
+    })
+  ).isRequired,
+};
 
 export default Rooms;
