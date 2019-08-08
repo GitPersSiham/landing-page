41196eb02659bc4665f94193a6d5e330dbd9038a
Jellfedora <julienlecointe@live.fr>
2019-08-08 14:24:30 +0200

    Add ajax

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './index.css';
 
-const Services = ({ elements }) => {
-    return (
-        <div className="service-component">
-            {
-                elements.map(({ id, titre, categorie }) => (
-                    <div className="service-container" key={id}>
-                        <p className="title">{id} : {titre}</p>
-                        <p className="categorie">Categorie : {categorie} </p>
-                    </div>
-                ))
-            }
-        </div>
+class Services extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            items: []
+        };
+    }
 
-    )
+    componentDidMount() {
+        fetch("http://localhost:3000/services")
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    // console.log(result);
+                    this.setState({
+                        isLoaded: true,
+                        items: result
+                    });
+                },
+                // Remarque : il est important de traiter les erreurs ici
+                // au lieu d'utiliser un bloc catch(), pour ne pas passer à la trappe
+                // des exceptions provenant de réels bugs du composant.
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error
+                    });
+                }
+            )
+    }
+
+    render() {
+        const { error, isLoaded, items } = this.state;
+        if (error) {
+            return <div>Erreur : {error.message}</div>;
+        } else if (!isLoaded) {
+            return <div>Chargement…</div>;
+        } else {
+
+            return (
+
+                <div className="service-component">
+                    {
+                        items.map(item => (
+                            <div className="service-container" key={item.id}>
+                                <p className="title">{item.id} : {item.title}</p>
+                                <p className="categorie">Categorie : {item.category} </p>
+                            </div>
+                        ))
+                    }
+
+                </div>
+
+
+            )
+
+        }
+    }
+
 }
-
 Services.propTypes = {
     titre: PropTypes.string,
     categorie: PropTypes.string,
 }
+
 
 export default Services;
