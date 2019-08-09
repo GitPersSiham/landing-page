82a48fb5540d5c182580c6fdd87c8b94f60a63ce
Anthony <tony.gorski13@gmail.com>
2019-08-09 14:31:02 +0200

    ajout du formulaire + boucle sur les testimonies

--- src/components/Testimonies/index.js
+++ src/components/Testimonies/index.js
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 // import { Card, Feed } from 'semantic-ui-react';
 import './index.css';
+import { Button, Header, Image, Modal } from 'semantic-ui-react';
+import {Checkbox, Form } from 'semantic-ui-react';
 
 const urlAPI = 'http://localhost:3000/testimonies'; 
-
 
 const Testimonies = () => {
 
@@ -13,52 +14,76 @@ const Testimonies = () => {
       .then(result => {
         const testimonies = result.data;
         console.log({testimonies});
-        setTestimonies({ 
-          ...testimonies,
-           name: testimonies.name + '!',
-           message: testimonies.message + '!',
-        });
-        //console.log("state enregistré");
+        setTestimonies(testimonies);
       })
       .catch(error => console.error(error));
   }, []);
 
   const [testimonies, setTestimonies] = useState([]);
 
+  const [testimonieIndex, changeTestimonie] = useState(0);
 
- const {name, avatar, message} = testimonies;
+  const handleClick = value => () => {
+    const newValue = 
+      testimonieIndex === 0 && value === -1 ? testimonies.length -1    // si index === 0 et value === -1 on va à la dernière case du tableau
+    : testimonieIndex === testimonies.length -1 && value === +1 ? 0    // sinon si index === dernière case et value === +1 on renvoie 0 (début)
+    : testimonieIndex + value;                                         // sinon testimonieIndex + value
+    changeTestimonie(newValue);
+  }
 
-  return (
-    <div className="ui people shape">
+ 
+  if (testimonies[testimonieIndex]) {
+    const { name, avatar, message } = testimonies[testimonieIndex];
+    return (
+      <div className="ui people shape">
         <div className="sides">
           <div className="side active">
-
               <div className="ui card">
                 <div className="image">
                   <img src= { avatar } alt="user-avatar" />
                 </div>
                 <div className="content">
-                  <div className="header">{ name }</div>
+                  <div className="header">
+                    { name }
+                  </div>
                   <div className="description">
-                   { message }
+                    { message }
                   </div>
                 </div>
               </div>
-            
-          
           </div>
           <div className="ui buttons"> 
-            <button className="ui button" ><i aria-hidden="true" className="left arrow icon"></i>Previous
-              
+            <button className="ui button" onClick={handleClick(-1)}>
+              <i aria-hidden="true" className="left arrow icon"></i>Previous
             </button>
             <div className="or" data-text=""></div>
-            <button className="ui button">Next
-              <i aria-hidden = "true" className = "right arrow icon" > </i>
+            <button className="ui button" onClick={handleClick(1)}>
+              Next<i aria-hidden = "true" className = "right arrow icon" > </i>
             </button>
           </div>
         </div>
+          
+        <Modal trigger={<Button>Ajouter un avis</Button>}>
+          <Form>
+            <Form.Field>
+              <label>First Name</label>
+              <input placeholder='Name' />
+            </Form.Field>
+            <Form.Field>
+              <label>Avatar</label>
+              <input placeholder='Avatar' />
+            </Form.Field>
+            <Form.Field>
+              <label>Message</label>
+              <textarea placeholder="Votre message"></textarea>
+            </Form.Field>
+            <Button type='submit'>Submit</Button>
+          </Form>
+        </Modal>   
       </div>
-  );
+      );
+  } else return 0;
+ 
 }
 
 export default Testimonies;
