a628c7a0c0960d1a66a241475f81ae1aad31b026
tonyoclock <tony@oclock.io>
2019-08-09 15:30:07 +0200

    weeekeeeeend

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
 import React, { useState, useEffect } from 'react';
 import Hello from '../Hello';
 import Services from '../Services';
@@ -20,58 +16,26 @@ const navElements = [
     {
         title: "Rooms",
         name: "rooms",
-<<<<<<< HEAD
-        Component: Rooms,
-        url: "/rooms",
-        isActive: true
-=======
         Component: Rooms
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
     },
     {
         title: "Services",
         name: "services",
-<<<<<<< HEAD
-        Component: Services,
-        url: "/services",
-        isActive: false
-=======
         Component: Services
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
     },
     {
         title: "Testimonies",
         name: "testimonies",
-<<<<<<< HEAD
-        Component: Testimonies,
-        url: "/testimonies",
-        isActive: false
-=======
         Component: Testimonies
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
     },
     {
         title: "DevMobileApp",
         name: "devmobileapp",
-<<<<<<< HEAD
-        Component: DevMobileApp,
-        url: "/devmobileapp",
-        isActive: false
-=======
         Component: DevMobileApp
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
     },
 ];
 
 function App() {
-<<<<<<< HEAD
-    const [elems, setElems] = useState(navElements)
-
-    return (
-        <div id="app">
-            <Navigation mode="horizontal" links={navElements} />
-            {elems.map(({ title, name, Component }) => (
-=======
     const [elements, setElements] = useState([])
 
     useEffect(() => {
@@ -82,7 +46,6 @@ function App() {
         <div>
             <Navigation mode="horizontal" links={elements} />
             {elements.map(({ title, name, Component }) => (
->>>>>>> 8ac866e47faaf92956d277856b3eef550ee587f4
                 <section id={name} key={name}>
                     <Component /> 
                 </section>
