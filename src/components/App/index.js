0f0a4b35672a76859518e46196d76d6a701b02ea
Live session user <mint@etudiant.oclock.io>
2019-08-09 14:50:47 +0200

    suite

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,59 @@
-
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Hello from '../Hello';
 import Services from '../Services';
 import servicesDatas from '../Services/data';
 import Navigation from '../Navigation';
 import Testimonies from '../Testimonies';
 import Rooms from '../Room';
-import navElements from '../Navigation/data';
+// import navElements from '../Navigation/data';
 import DevMobileApp from '../DevMobileApp';
 import mobileAppElements from '../DevMobileApp/data';
 
 // Transformer le composant en class
 // Utiliser le state pour stocker les données
 
-const App = () => {
+const navElements = [
+    {
+        title: "Rooms",
+        name: "rooms",
+        Component: Rooms,
+        url: "/rooms",
+        isActive: true
+    },
+    {
+        title: "Services",
+        name: "services",
+        Component: Services,
+        url: "/services",
+        isActive: false
+    },
+    {
+        title: "Testimonies",
+        name: "testimonies",
+        Component: Testimonies,
+        url: "/testimonies",
+        isActive: false
+    },
+    {
+        title: "DevMobileApp",
+        name: "devmobileapp",
+        Component: DevMobileApp,
+        url: "/devmobileapp",
+        isActive: false
+    },
+];
+
+function App() {
+    const [elems, setElems] = useState(navElements)
+
     return (
         <div>
             <Navigation mode="horizontal" links={navElements} />
-            {/* 
-                faire une boucle sur les  datas (du state)
-                Pour chaque élément fabriquer une section qui
-                affichera la composant demandé
-            */}
+            {elems.map(({ title, name, Component }) => (
+                <section id={name} key={name}>
+                    <Component /> 
+                </section>
+            ))}
             <Rooms />
             <Services elements={servicesDatas} />
             <Testimonies />
