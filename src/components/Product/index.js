c73c582595eee7c5cd90e5c1dbb65189ca7197b5
GitPersSiham <lahouali.siham@outlook.fr>
2020-08-16 14:15:37 +0200

    ajout adresse de l'api

--- src/components/Product/index.js
+++ src/components/Product/index.js
@@ -17,7 +17,8 @@ const Products = () => {
   });
 
   const fetchProducts = () => {
-    axios.get("http://localhost:3000/products").then(res =>{
+   /* axios.get("http://localhost:3000/products").then(res =>{*/
+   axios.get("https://app-service-28.herokuapp.com/products").then(res =>{ 
       let allProducts = res.data;
       let currentMiddleDisplayIndex = Math.floor(state.numberOfMultipleProducts / 2);
       let currentProductsDisplayed = allProducts.slice(0, state.numberOfMultipleProducts);
